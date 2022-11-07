5e8e64707f2e2e336b8f761614175c7b51a875b9
Schartier Isaac <dynamique.zak@gmail.com>
2022-11-07 15:27:11 +0100

    First init

---
+++ EniDemo/EniDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EniDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            /*
+            Label LabelTest = new Label { Text = "Test",
+                Margin = new Thickness(10, 10, 10, 10),
+                BackgroundColor = Color.FromHex("#")
+            };
+
+            this.divParent.Children.Add(LabelTest);
+            */
+        }
+    }
+}
